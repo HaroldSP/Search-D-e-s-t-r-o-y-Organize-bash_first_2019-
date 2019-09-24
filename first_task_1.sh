dff3c0abce67ae1ad39eb44acb3cb995a6f0e007
HaroldSP <55085987+HaroldSP@users.noreply.github.com>
2019-09-24 11:52:14 +0300

    Update first_task_1.sh

--- first_task_1.sh
+++ first_task_1.sh
@@ -3,22 +3,44 @@
 function task {				# функция для обработки файлов в директории
 for f in "$dir1"/*			# цикл для перебора файлов
 do
-	local file="${f##*/}"		# выделение имени файла из абсолютного пути
-	if [[ -d "$f" ]]; then		# проверка - является ли файл директорией
+	if test -d "$f" ; then  # проверка - является ли файл директорией (аналогично "[[]]" )
 		dir1=$f			# переход на уровень ниже
 		task			# рекурсивный вызов функции для обработки файлов во вложенной директории
-	else				
-		local name="${file%.[^.]*}"			# выделение имени файла с помощью регулярного выражения
-		local extension="${file:${#name} + 1}"		# выделение расширения файла
-		local size=$(wc -c "$f" | awk '{print$1}')			# получение размера файла в байтах (выделение первого аргумента команды wc -c)
-		local duration=$(ffprobe -i "$f" -show_entries format=duration -v quiet -of csv="p=0" -sexagesimal)	# получение длины видео с помощью библиотеки ffmpeg
-		echo -e "$name \t $extension \t $size "B" \t $duration" >> result.xls		# составление строки вывода для файла .xls
+	else	
+		local file="${f##*/}"		
+# выделение имени файла с расширением из абсолютного пути
+			
+		local name="${file%.[^.]*}"		
+# выделение имени файла без расширения с помощью регулярного выражения; можно также $(echo "$filename" | sed 's/\.[^.]*$//')
+
+		local extension="${f##*.}"		
+# выделение расширения файла; 
+#можно также
+		#local extension=$(echo "$file" | sed 's/^.*\.//') #с помощью sed
+
+		local lastchange=$(ls -Rl "$f" | awk '{print $6, $7, $8}') #$8 ставит время, если в это году, в противном случае ставит год
+#можно также:
+		#local lastchange=$(date -Rr "$f") #выводить время последнего изменения в соответствии с RFC-2822
+#можно также:
+		#local lastchange=$(stat -c %y "$f") #пример: 2019-09-23 21:56:05.492022112 +0300
+
+		local size=$(du -k "$f" | awk '{print $1}') 
+#размер в Кб, -m размер в мб, awk выводит только первое поле; 
+#можно также: 
+		#local size=$(wc -c "$f" | awk '{print$1}') #получение размера файла в байтах (выделение первого аргумента команды wc -c)
+
+		local duration=$(ffprobe -i "$f" -show_entries format=duration -v quiet -of csv="p=0" -sexagesimal) 
+#получение длины видео с помощью библиотеки ffmpeg
+#можно также: 
+		#local duration=$(ffmpeg -i "$f" 2>&1 | grep Duration | awk '{print $2}')  #компактнее, но с запятой в конце записи 
+
+		echo -e "$name \t $extension \t $lastchange \t $size "kb" \t $duration" >> parser.xls		# составление строки вывода для файла .xls
 		dir1=$dir		#возврат в первоначальную директорию
 	fi
 done
 }
 
-rm /Users/aleksejkozlov/result.xls		# удаление выходного файла, если такой существует
+rm /home/egor/parser.xls		# удаление выходного файла, если существует
 echo -n "Write a full path to the directory:"		# вывод в командную строку предложения о вводе нужной папки для перебора
 read dir		# считывание данных из командной строки
 dir1=$dir		# переменная для использования в функции
